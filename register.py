dda84b7ace86a0473fc5fae4f6be1610c3e5353d
Taavi Annus <Taavi@MacBook-Pro.lan>
2019-10-26 10:20:43 +0300

    third cycle

--- register.py
+++ register.py
@@ -3,7 +3,7 @@
 @system: Laboratory Information System for Mt. Sinai Hospital
 '''
 
-#Import regex, csv and unittest
+#Import regex, csv and array
 import re, csv
 
 #Set email regex
@@ -45,7 +45,7 @@ def rolecheck(role):
     else:
         return 8
 
-#Error message handling
+#Print corresponding error message
 def error_message(error):
     message = {
         1: 'Email is invalid!',
@@ -62,49 +62,48 @@ def error_message(error):
 
 #Register user
 def register_user(email, password, password2, firstname, lastname, role):
-    save_db=email, password, firstname, lastname, role
-    file=open('./users.csv','a')
-    file.write(str(save_db))
-    file.close
-    print('Success you are now registered!')
+    error = 0
+    data = [email, password, password2, firstname, lastname, role]
+    #check if any field is empty
+    for i in data:
+        result = emptycheck(i)
+        if (result != 0):
+            error_message(result)
+            error = 1
+    #check if email is valid
+    if (emailcheck(email) != 0):
+        error_message(emailcheck(email))
+        error = 1
+    #check if password is valid
+    pcheck = passwordcheck(password, password2, firstname, lastname)
+    if (pcheck != 0):
+        error_message(pcheck)
+        error = 1
+    #check if role is valid
+    rcheck = rolecheck(role)
+    if (rcheck != 0):
+        error_message(rcheck)
+        error = 1
+
+    if (error == 0):
+        save_db=email, password, firstname, lastname, role
+        file=open('./users.csv','a')
+        file.write(str(save_db))
+        file.close
+        print('Success you are now registered!')
+        return True
+    else:
+        return False
 
 #Registration form  
 def register_form():
     print('Welcome to Mt. Sinai Hospital LIMS system registration page! Please fill in the form.')
-    while True:
-        email=input('Email:')
-        if (emailcheck(email) == 0):
-            break
-        else:
-            error_message(emailcheck(email))
-    while True:
-        firstname=input('Firstname:')
-        if (emptycheck(firstname) == 0):
-            break
-        else:
-            error_message(emptycheck(firstname))
-    while True:
-        lastname=input('Lastname:')
-        if (emptycheck(lastname) == 0):
-            break
-        else:
-            error_message(emptycheck(lastname))
-    while True:
-        password=input('Password: ')
-        password2=input('Repeat password:')
-        if (passwordcheck(password, password2, firstname, lastname) == 0):
-            break
-        else:
-            error_message(passwordcheck(password, password2, firstname, lastname))
-    while True:
-        role=input('Role in the hospital:')
-        if (emptycheck(role) != 0 or rolecheck(role) != 0):
-            if (emptycheck(role) == 0):
-                error_message(rolecheck(role))
-            else:
-                error_message(emptycheck(role))
-        else:
-            break
+    email=input('Email:')
+    firstname=input('Firstname:')
+    lastname=input('Lastname:')
+    password=input('Password: ')
+    password2=input('Repeat password:')
+    role=input('Role in the hospital:')
     register_user(email, password, password2, firstname, lastname, role)
 
 if __name__ == "__main__":
--- register_test.py
+++ register_test.py
@@ -3,42 +3,42 @@
 @system: Laboratory Information System for Mt. Sinai Hospital
 '''
 
-#Import regex, csv and unittest
-import re, csv, unittest
+#Import regex and unittest
+import re, unittest
 
 #import register.py
 import register as r
 
-#Testing functions emptycheck(input), passwordcheck(password, password2, firstname, lastname), emailcheck(email) and rolecheck(role)
+#Testing function for registering users: register_user(email, password, password2, firstname, lastname, role)
 class TestMethods(unittest.TestCase):
     def test_firstname1(self):
-        self.assertEquals(r.emptycheck('Roman'), 0, 'FR001A - firstname is valid')
+        self.assertTrue(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson', 'Doctor'), 'FR001A - firstname is valid')
     def test_firstname2(self):
-        self.assertEquals(r.emptycheck(''), 7, 'FR001A - firstname is empty')
+        self.assertFalse(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', '', 'Jackson', 'Doctor'), 'FR001A - firstname is empty')
     def test_lastname1(self):
-        self.assertEquals(r.emptycheck('Jackson'), 0, 'FR001A - lastname is valid')
+        self.assertTrue(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson', 'Doctor'), 'FR001A - lastname is valid')
     def test_lastname2(self):
-        self.assertEquals(r.emptycheck(''), 7, 'FR001A - lastname is empty')
+        self.assertFalse(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', '', 'Doctor'), 'FR001A - lastname is empty')
     def test_password1(self):
-        self.assertEquals(r.passwordcheck('KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson'), 0, 'FR001B, FR001C - password is valid')
+        self.assertTrue(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson', 'Doctor'), 'FR001B, FR001C - password is valid')
     def test_password2(self):
-        self.assertEquals(r.passwordcheck('Khayyam', 'Khayyam', 'Khayyam', 'Jackson'), 3, 'FR001C - Password is firstname')
+        self.assertFalse(r.register_user('taavi@taltech.ee', 'Khayyam', 'Khayyam', 'Khayyam', 'Jackson', 'Doctor'), 'FR001B - Password is firstname')
     def test_password3(self):
-        self.assertEquals(r.passwordcheck('Jackson', 'Jackson', 'Roman', 'Jackson'), 2, 'FR001B - Password is lastname')
+        self.assertFalse(r.register_user('taavi@taltech.ee', 'Jackson', 'Jackson', 'Roman', 'Jackson', 'Doctor'), 'FR001B - Password is lastname')
     def test_password4(self):
-        self.assertFalse(r.passwordcheck('1234', '1234', 'Roman', 'Jackson'), 'FR001C - password too short')
-    def test_password4(self):
-        self.assertEquals(r.passwordcheck('KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson'), 5, 'FR001D - Passwords match')
+        self.assertFalse(r.register_user('taavi@taltech.ee', '1234', '1234', 'Khayyam', 'Jackson', 'Doctor'), 'FR001C - Password too short')
     def test_password5(self):
-        self.assertEquals(r.passwordcheck('KhayyamPassword', 'Khayyam', 'Roman', 'Jackson'), 6, 'FR001D - Passwords do not match')
+        self.assertTrue(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson', 'Doctor'), 'FR001D - Passwords match')
+    def test_password6(self):
+        self.assertFalse(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'Khayyam', 'Roman', 'Jackson', 'Doctor'), 'FR001D - Passwords do not match')
     def test_email1(self):
-        self.assertEquals(r.emailcheck('taavi@taltech.ee'), 0, 'FR001E - email is valid')
+        self.assertTrue(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson', 'Doctor'), 'FR001E - email is valid')
     def test_email2(self):
-        self.assertEquals(r.emailcheck('taavi@taltech@ee'), 1, 'FR001E - email is invalid')
+        self.assertFalse(r.register_user('taavi@taltech@ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson', 'Doctor'), 'FR001E - email is invalid')
     def test_role1(self):
-        self.assertEquals(r.rolecheck('Doctor'), 0, 'FR001F - role is valid')
+       self.assertTrue(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson', 'Doctor'), 'FR001F - role is valid')
     def test_role2(self):
-        self.assertEquals(r.rolecheck('Plumber'), 8, 'FR001F - role is invalid')
+        self.assertFalse(r.register_user('taavi@taltech.ee', 'KhayyamPassword', 'KhayyamPassword', 'Roman', 'Jackson', 'Plumber'), 'FR001F - role is invalid')
 
 if __name__ == "__main__":
     unittest.main()
